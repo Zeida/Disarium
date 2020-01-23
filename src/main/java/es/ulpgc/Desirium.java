cfbf38715df70f88c6ef623030057bc322c1a427
zeida <zeidaromen97@gmail.com>
2020-01-23 11:07:29 +0000

    Initial commit

---
+++ src/main/java/es/ulpgc/Desirium.java
@@ -0,0 +1,13 @@
+package es.ulpgc;
+
+/**
+ * Hello world!
+ *
+ */
+public class Desirium
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
